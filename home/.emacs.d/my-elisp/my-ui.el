121abb774054344fd25dacb375b02fc2f3374082
maanuair <emmanuel.roubion@gmail.com>
2016-10-13 14:29:05 +0200

    Changed default font to Cousine for Powerline, used alphabetical order in list

---
+++ home/.emacs.d/my-elisp/my-ui.el
@@ -0,0 +1,82 @@
+;; Behaviors
+;;;;;;;;;;;;
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+
+;; Use recent files
+(require 'recentf)
+(recentf-mode 1)
+
+;; Customize behavior
+(load-library "paren")    ;; We use paren lib...
+(show-paren-mode 1 )      ;; Visually match parentheses
+
+;; Custom keyboard shortcuts
+(global-set-key [(meta g)] 'goto-line)
+(global-set-key (quote [f5]) 'reformat)
+(global-set-key (quote [f12]) 'auto-revert-tail-mode)
+
+;; Specific mac beahviors
+(when (is-mac)
+  (setq
+   delete-by-moving-to-trash t     ;; Move to trash when deleting stuff
+   trash-directory "~/.Trash/"     ;; Trash folder on OSX is ~/.Trash/)
+   mac-right-option-modifier 'none ;; Make the right option (Alt) key native
+   ))
+
+;; Specific Win beahviors
+(when (is-win)
+  (setq
+   delete-by-moving-to-trash t     ;; Move to trash when deleting stuff
+   ))
+
+;; Enable back the commands considered confusing to upper/lower case region
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+
+;; Aappearance
+;;;;;;;;;;;;;;
+
+(setq
+ apropos-do-all t               ;; More extensive search in apropos
+ initial-scratch-message nil    ;; Remove scratch message
+ inhibit-startup-screen t       ;; Remove start-up screen
+ font-lock-maximum-decoration t ;; Use maximum decoration
+ frame-title-format (list '(buffer-file-name "%f" (dired-directory dired-directory "%b")) " (" user-login-name "@" system-name ")") ;; Sets the frame title
+ )
+(column-number-mode 1)        ;; Display column number
+(display-battery-mode 1)      ;; Show battery level
+(global-hl-line-mode 1)       ;; Highlight current line
+(global-font-lock-mode t)     ;; Toggle font Lock mode in all buffers
+(size-indication-mode 1)      ;; Show size of buffer
+(toggle-indicate-empty-lines) ;; Show empty lines
+(tool-bar-mode -1)            ;; Remove toolbar
+(transient-mark-mode t)       ;; Highlight active region
+
+;; Use visible bell, customized
+(defun my-terminal-visible-bell ()
+  "A friendlier visual bell effect."
+  (invert-face 'mode-line)
+  (run-with-timer 0.1 nil 'invert-face 'mode-line))
+(setq
+ ring-bell-function 'my-terminal-visible-bell
+ visible-bell t
+ )
+
+;; Use solarized theme, but stick to my preferred fonts
+(load-theme 'solarized-dark t)
+(when (is-mac)
+  ;;(add-to-list 'default-frame-alist '(font . "Monaco-11"))
+  ;;(set-face-attribute 'default t :font "Monaco-11"))
+  (add-to-list 'default-frame-alist '(font . "Cousine for Powerline-12"))
+  (set-face-attribute 'default t :font "Cousine for Powerline-12"))
+(when (is-win)
+  (add-to-list 'default-frame-alist '(font . "Inconsolata-10"))
+  (set-face-attribute 'default t :font "Inconsolata-10"))
+
+(provide 'my-ui)
+				   
