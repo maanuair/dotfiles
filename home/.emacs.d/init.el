c71240aba1bc8eaf62be71742ec300777a7da47f
maanuair <emmanuel.roubion@gmail.com>
2019-09-06 18:38:40 +0200

    chore: Re-add MELPA since some packages are only there

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -24,7 +24,7 @@
 ;; Initialize my packages...
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (require 'my-packages)
 
