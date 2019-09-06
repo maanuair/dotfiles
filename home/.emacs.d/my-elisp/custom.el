7aa37dc177373bbdc96497c7e01668a2ebaaa06b
maanuair <emmanuel.roubion@gmail.com>
2019-09-06 18:39:19 +0200

    chore: Small maintenance

--- home/.emacs.d/my-elisp/custom.el
+++ home/.emacs.d/my-elisp/custom.el
@@ -19,9 +19,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" default)))
  '(package-selected-packages
    (quote
-    (bug-hunter all-the-icons doom-modeline editorconfig emmet-mode groovy-mode hackernews helm js2-mode json-mode lua-mode markdown-mode magit mocha org-kanban ox-hugo restclient slime solarized-theme web-mode zenburn-theme))))
+    (org-cliplink bug-hunter all-the-icons doom-modeline editorconfig emmet-mode groovy-mode hackernews helm js2-mode json-mode lua-mode markdown-mode magit mocha org-kanban ox-hugo restclient slime solarized-theme web-mode zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
